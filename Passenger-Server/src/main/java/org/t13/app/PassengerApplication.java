48c53912e1e788acc378f2a4426ab934c52c107d
thanish13 <thanishmalai13@gmail.com>
2025-11-13 21:27:59 +0530

    Flight and Pssenger server running

--- Passenger-Server/src/main/java/org/t13/app/PassengerApplication.java
+++ Passenger-Server/src/main/java/org/t13/app/PassengerApplication.java
@@ -1,11 +1,19 @@
 package org.t13.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class,
+		DataSourceAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
 public class PassengerApplication {
 
 	public static void main(String[] args) {
