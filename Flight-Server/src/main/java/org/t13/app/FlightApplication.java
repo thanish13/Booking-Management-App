9503b9a62dee7eff854b4eaa8de8d7dd4370701e
thanish13 <thanishmalai13@gmail.com>
2025-12-14 16:43:16 +0530

    Otel changes

--- Flight-Server/src/main/java/org/t13/app/FlightApplication.java
+++ Flight-Server/src/main/java/org/t13/app/FlightApplication.java
@@ -1,5 +1,6 @@
 package org.t13.app;
 
+import io.opentelemetry.instrumentation.spring.autoconfigure.internal.instrumentation.webmvc.SpringWebMvc6InstrumentationAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class,
 		DataSourceAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
+		org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class,
+		SpringWebMvc6InstrumentationAutoConfiguration.class})
 public class FlightApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightApplication.class, args);
