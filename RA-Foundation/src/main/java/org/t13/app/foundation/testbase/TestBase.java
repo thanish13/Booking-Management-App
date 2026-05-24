db2e4e5c2ae20dd3592099744f933e4c5e66da61
thanish13 <thanishmalai13@gmail.com>
2026-05-24 20:29:55 +0530

    JDK 25

--- RA-Foundation/src/main/java/org/t13/app/foundation/testbase/TestBase.java
+++ RA-Foundation/src/main/java/org/t13/app/foundation/testbase/TestBase.java
@@ -3,6 +3,7 @@ package org.t13.app.foundation.testbase;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
